eb5e17bd4817fd2131eb0959e5bf041cda29dd63
Lucian <luciangabrielilie@gmail.com>
2022-08-19 18:22:12 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+using Microsoft.Win32;
+using System.Runtime.Versioning;
+
+namespace restfulhwinfo
+{
+    [SupportedOSPlatform("windows")]
+    class Program
+    {
+        const string Url = "http://localhost:60000";
+        const string KeyName = "SOFTWARE\\HWiNFO64\\VSB";
+        static RegistryKey RegistryKey = Registry.CurrentUser.OpenSubKey(KeyName) ?? throw new InvalidOperationException("Registry key not found!");
+
+        static void Main(string[] args)
+        {
+            var app = WebApplication.CreateBuilder(args).Build();
+            app.MapGet("/", () => GetValueNames());
+
+            app.Run(Url);
+        }
+
+        private static object GetValueNames()
+        {
+            return RegistryKey
+                        .GetValueNames()
+                        .ToList()
+                        .GroupBy(x => string.Join("", x.Reverse().TakeWhile(c => char.IsDigit(c)).Reverse()))
+                        .ToDictionary(x => x.Key, x => x.ToList().Select(l => new { Label = l, Value = RegistryKey.GetValue(l) }));
+        }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,19 @@
+# restfulhwinfo
+
+restfulhwinfo is a Windows Kotlin Native program for serving HWiNFO sensor readings through RESTful API.
+
+## Running from source code
+Execute the following command:
+```bash
+dotnet run
+```
+
+Server will be available at http://localhost:60000.
+
+## Releasing
+Run the following command:
+```bash
+dotnet publish -r win10-x64 -c Release /p:PublishSingleFile=true --self-contained
+```
+
+The server can be started by executing the generated .exe file found here: `<project_path>/bin/Release/net6.0/win10-x64/publish/restfulhwinfo.exe`
